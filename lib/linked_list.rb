455c7e7c7e810dda43c7b2e1b6b8d6ef16fb6525
JennicaStiehl <jennica.stiehl@gmail.com>
2019-04-26 17:32:08 -0600

    adds iteration2

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -23,10 +23,29 @@ class LinkedList
     @to_string << (data + ' ')
   end
 
+  def prepend(data)
+    node = Node.new(data)
+    node.next_node = head
+    head = node
+    @count +=1
+    @to_string << (data + ' ')
+  end
+
   def find_tail
     node = @head
     return node if !node.next_node
     return node if !node.next_node while (node = node.next_node)
   end
+
+  def find(value)
+    binding.pry
+    node = @head
+    return false if !node.next_node
+    return node  if node.value == value
+    while (node = node.next_node)
+      return node if node.value == value
+    end
+  end
+
 
 end
